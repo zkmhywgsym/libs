116be51a4d187439ebe7978e67d831ca616c1268
Administrator <Administrator@hyq>
2015-06-18 08:50:55 +0800

    init

--- libZXingScanner/src/com/zijunlin/Zxing/Demo/CreateQRImageTest.java
+++ libZXingScanner/src/com/zijunlin/Zxing/Demo/CreateQRImageTest.java
@@ -1,8 +1,10 @@
 package com.zijunlin.Zxing.Demo;
 
+import java.io.File;
 import java.util.Hashtable;
 
 import android.graphics.Bitmap;
+import android.text.TextUtils;
 import android.widget.ImageView;
 
 import com.google.zxing.BarcodeFormat;
@@ -11,41 +13,42 @@ import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 /**
- * @Àà¹¦ÄÜËµÃ÷: Éú³É¶şÎ¬ÂëÍ¼Æ¬Ê¾Àı
- * @×÷Õß:ÈÄÕıÓÂ
- * @Ê±¼ä:2013-4-18ÉÏÎç11:10:42
- * @°æ±¾:V1.0
+ * @ç±»åŠŸèƒ½è¯´æ˜: ç”ŸæˆäºŒç»´ç å›¾ç‰‡ç¤ºä¾‹
+ * @ä½œè€…:é¥¶æ­£å‹‡
+ * @æ—¶é—´:2013-4-18ä¸Šåˆ11:10:42
+ * @ç‰ˆæœ¬:V1.0
  */
 public class CreateQRImageTest
 {
 	private ImageView sweepIV;
 	private int QR_WIDTH = 200, QR_HEIGHT = 200;
+	
 	/**
-	 * @·½·¨¹¦ÄÜËµÃ÷: Éú³É¶şÎ¬ÂëÍ¼Æ¬,Êµ¼ÊÊ¹ÓÃÊ±Òª³õÊ¼»¯sweepIV,²»È»»á±¨¿ÕÖ¸Õë´íÎó
-	 * @×÷Õß:ÈÄÕıÓÂ
-	 * @Ê±¼ä:2013-4-18ÉÏÎç11:14:16
-	 * @²ÎÊı: @param url Òª×ª»»µÄµØÖ·»ò×Ö·û´®,¿ÉÒÔÊÇÖĞÎÄ
+	 * @æ–¹æ³•åŠŸèƒ½è¯´æ˜: ç”ŸæˆäºŒç»´ç å›¾ç‰‡,å®é™…ä½¿ç”¨æ—¶è¦åˆå§‹åŒ–sweepIV,ä¸ç„¶ä¼šæŠ¥ç©ºæŒ‡é’ˆé”™è¯¯
+	 * @ä½œè€…:é¥¶æ­£å‹‡
+	 * @æ—¶é—´:2013-4-18ä¸Šåˆ11:14:16
+	 * @å‚æ•°: @param url è¦è½¬æ¢çš„åœ°å€æˆ–å­—ç¬¦ä¸²,å¯ä»¥æ˜¯ä¸­æ–‡
 	 * @return void
 	 * @throws
 	 */
 	
-	//Òª×ª»»µÄµØÖ·»ò×Ö·û´®,¿ÉÒÔÊÇÖĞÎÄ
-	public void createQRImage(String url)
+	//è¦è½¬æ¢çš„åœ°å€æˆ–å­—ç¬¦ä¸²,å¯ä»¥æ˜¯ä¸­æ–‡
+	public void createQRImage(String url,String path)
 	{
 		try
 		{
-			//ÅĞ¶ÏURLºÏ·¨ĞÔ
+			//åˆ¤æ–­URLåˆæ³•æ€§
 			if (url == null || "".equals(url) || url.length() < 1)
 			{
 				return;
 			}
 			Hashtable<EncodeHintType, String> hints = new Hashtable<EncodeHintType, String>();
 			hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
-			//Í¼ÏñÊı¾İ×ª»»£¬Ê¹ÓÃÁË¾ØÕó×ª»»
+			//å›¾åƒæ•°æ®è½¬æ¢ï¼Œä½¿ç”¨äº†çŸ©é˜µè½¬æ¢
 			BitMatrix bitMatrix = new QRCodeWriter().encode(url, BarcodeFormat.QR_CODE, QR_WIDTH, QR_HEIGHT, hints);
 			int[] pixels = new int[QR_WIDTH * QR_HEIGHT];
-			//ÏÂÃæÕâÀï°´ÕÕ¶şÎ¬ÂëµÄËã·¨£¬Öğ¸öÉú³É¶şÎ¬ÂëµÄÍ¼Æ¬£¬
-			//Á½¸öforÑ­»·ÊÇÍ¼Æ¬ºáÁĞÉ¨ÃèµÄ½á¹û
+			//ä¸‹é¢è¿™é‡ŒæŒ‰ç…§äºŒç»´ç çš„ç®—æ³•ï¼Œé€ä¸ªç”ŸæˆäºŒç»´ç çš„å›¾ç‰‡ï¼Œ
+			//ä¸¤ä¸ªforå¾ªç¯æ˜¯å›¾ç‰‡æ¨ªåˆ—æ‰«æçš„ç»“æœ
 			for (int y = 0; y < QR_HEIGHT; y++)
 			{
 				for (int x = 0; x < QR_WIDTH; x++)
@@ -60,10 +63,20 @@ public class CreateQRImageTest
 					}
 				}
 			}
-			//Éú³É¶şÎ¬ÂëÍ¼Æ¬µÄ¸ñÊ½£¬Ê¹ÓÃARGB_8888
+			//ç”ŸæˆäºŒç»´ç å›¾ç‰‡çš„æ ¼å¼ï¼Œä½¿ç”¨ARGB_8888
 			Bitmap bitmap = Bitmap.createBitmap(QR_WIDTH, QR_HEIGHT, Bitmap.Config.ARGB_8888);
 			bitmap.setPixels(pixels, 0, QR_WIDTH, 0, 0, QR_WIDTH, QR_HEIGHT);
-			//ÏÔÊ¾µ½Ò»¸öImageViewÉÏÃæ
+			//æ˜¾ç¤ºåˆ°ä¸€ä¸ªImageViewä¸Šé¢
+			if (!TextUtils.isEmpty(path)) {
+				File dir=new File(path);
+				if (!dir.exists()) {
+					dir.mkdirs();
+				}else{
+					if(dir.isDirectory()){
+						
+					}
+				}
+			}
 			sweepIV.setImageBitmap(bitmap);
 		}
 		catch (WriterException e)
